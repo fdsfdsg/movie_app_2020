10d6dedc5f968d55f29c71bccedf933e2a90fd68
park sangbeom <smctkdqja@gmail.com>
2020-01-26 20:59:09 +0900

    update

---
+++ src/routes/About.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./About.css"
+
+function About() {
+
+    return (
+        <div className="about__container">
+            <span>
+                노마더 코드 리액트로 간단한 웹 서비스 만들어보기
+            </span>
+            <span>- Auto: 박상붐, 2020</span>
+        </div>
+    );
+
+}
+
+export default About;
